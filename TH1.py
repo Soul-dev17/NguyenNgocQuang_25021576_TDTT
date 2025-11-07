27a811855e2e44abb5b34590d47a4840ee2ba6ab
Soul <nguyenquang.soul@gmail.com>
2025-11-07 08:59:19 +0700

    Delete TH1.py

--- TH1.py
+++
@@ -1,3 +0,0 @@
-a1, b1, c1, a2, b2, a3 = int(input("nhap diem a1:")), int(input("nhap diem b1:")), int(input("nhap diem c1:")), int(input("nhap diem a2:")), int(input("nhap diem b2:")), int(input("nhap diem a3:"))
-diemtrungbinh=(a1 + b1 + c1 + (a2+b2)*2 + a3*3)/10
-print("diem trung binh la:", diemtrungbinh)
